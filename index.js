2bbea31ef901dcef864ffea042e74f929ded4bd7
lenor21 <dejesusronel29@gmail.com>
2022-09-20 03:10:23 +0800

    modified files

--- index.js
+++ index.js
@@ -5,42 +5,57 @@ const resetButton = document.querySelector("#resetButton");
 const myResult = document.querySelector("#myResult");
 const myClassification = document.querySelector("#myClassification");
 
+calculateButton.style.backgroundColor = "lightgreen";
+resetButton.style.backgroundColor = "lightblue";
+
 calculateButton.addEventListener("click", () => {
   let height = Math.pow(heightInput.value * 0.01, 2);
   let weight = weightInput.value;
   let bmi = weight / height;
 
   emptyInputs();
-  resultNan();
 
   function emptyInputs() {
     if (heightInput.value == "" && weightInput.value == "") {
       alert("Please complete both inputs");
       bmi = "";
-      myClassification.textContent = `Classification:`;
+      myClassification.textContent = "";
     } else if (heightInput.value == "") {
       alert("Please complete the height input");
     } else if (weightInput.value == "") {
       alert("Please complete the weight input");
     } else if (heightInput.value != "" && weightInput.value != "") {
-      myResult.textContent = `Result: ${Math.round(bmi * 100) / 100}`;
+      myResult.textContent = `${Math.round(bmi * 100) / 100}`;
       bmiClassification();
+      resultNan();
     }
   }
 
   function bmiClassification() {
     if (bmi < 18.5) {
-      myClassification.textContent = `Classification: Underweight`;
+      myClassification.textContent = " Underweight";
+      myClassification.style.color = "orange";
+      myResult.style.color = "orange";
     } else if (bmi >= 18.5 && bmi <= 24.9) {
-      myClassification.textContent = `Classification: Normal weight`;
+      myClassification.textContent = " Normal weight";
+      myClassification.style.color = "green";
+      myResult.style.color = "green";
     } else if (bmi >= 25 && bmi <= 29.9) {
-      myClassification.textContent = `Classification: Overweight`;
+      myClassification.textContent = ` Overweight`;
+      myClassification.style.color = "orange";
+      myResult.style.color = "orange";
     } else if (bmi >= 30 && bmi <= 34.9) {
-      myClassification.textContent = `Classification: Obesity class 1`;
+      myClassification.textContent = ` Obesity class 1`;
+      myClassification.style.color = "red";
+      myResult.style.color = "red";
     } else if (bmi >= 35 && bmi <= 39.9) {
-      myClassification.textContent = `Classification: Obesity class 2`;
+      myClassification.textContent = ` Obesity class 2`;
+      myClassification.style.color = "red";
+      myResult.style.color = "red";
     } else if (bmi >= 40) {
-      myClassification.textContent = `Classification: Obesity class 3`;
+      myClassification.textContent = ` Obesity class 3`;
+      myClassification.style.color = "red";
+      myResult.style.color = "red";
     }
   }
 
@@ -48,7 +63,7 @@ calculateButton.addEventListener("click", () => {
     if (isNaN(bmi)) {
       heightInput.value = "";
       weightInput.value = "";
-      myResult.textContent = `Result:`;
+      myResult.textContent = "";
       alert(" Invalid inputs");
     }
   }
@@ -57,6 +72,6 @@ calculateButton.addEventListener("click", () => {
 resetButton.addEventListener("click", () => {
   heightInput.value = "";
   weightInput.value = "";
-  myResult.textContent = `Result:`;
-  myClassification.textContent = `Classification:`;
+  myResult.textContent = "";
+  myClassification.textContent = "";
 });
